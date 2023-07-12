52e37a4843b260f783c60a2db874c6e94615c845
Sam Thrasher <samthrasher9@gmail.com>
2023-07-12 14:37:13 -0500

    Added get_all_orders

---
+++ views/order_requests.py
@@ -0,0 +1,17 @@
+ORDERS = [
+    {
+        "metalId": 1,
+        "sizeId": 4,
+        "styleId": 2,
+        "id": 1
+    },
+    {
+        "metalId": 3,
+        "sizeId": 5,
+        "styleId": 3,
+        "id": 2
+    }
+]
+
+def get_all_orders():
+    return ORDERS
